f4cc0acb63a7c27251afe71a8f278a581ab14fc2
Cecilia Casadei <cecilia.casadei@psi.ch>
2016-08-09 15:11:34 +0200

    fixed numpy includes

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 from distutils.core import setup
 from Cython.Build import cythonize
+import numpy
+
+print numpy.get_include()
 
 setup(
-    ext_modules = cythonize("transform_calculateCCs.pyx")
+    ext_modules = cythonize("transform_calculateCCs.pyx", ),
+    include_dirs=[numpy.get_include(), ]
 )
 
 
