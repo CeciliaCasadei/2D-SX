34748668d423cdcd9a5cc7f9a8a25fd3a5888105
casadei_c <cecilia.casadei@psi.ch>
2017-06-21 17:34:27 +0200

    Improved I_ellipse vs q plot.

--- plot_I_ellipse_vs_q.py
+++ plot_I_ellipse_vs_q.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 import numpy
 import matplotlib.pyplot
-outputFolder = './Output_imageSums_moduleDisplacements_sigmaFits'
+
+runNumber = '0127'
+
+outputFolder = './Output_r%s/Output_imageSums_moduleDisplacements_sigmaFits'%runNumber
 cellSize = 62.45
 directCell = cellSize * numpy.matrix([[1, numpy.cos(2*numpy.pi/3)],[0, numpy.sin(2*numpy.pi/3)]]) # A
 reciprocalCellRows = 2 * numpy.pi * directCell.I                     
@@ -21,12 +24,37 @@ for data_line in data:
     
     qs.append(q)
     Is_ellipse.append(I_ellipse)
+    
+    
+# BINNING    
+qs_bins = []
+Is_ellipse_bins = [] 
+
+
+bins = numpy.linspace(min(qs), max(qs), 25)
+for i in range(0, len(bins)-1):
+    left_q  = bins[i]
+    right_q = bins[i+1]
+    q_bin               = [qs[i]         for i in range(0, len(qs)) if left_q <= qs[i] <= right_q]    
+    I_ellipse_bin       = [Is_ellipse[i] for i in range(0, len(qs)) if left_q <= qs[i] <= right_q]
+   
+    print len(q_bin)
+    #q_avg_bin = numpy.average(q_bin)
+    q_mid_bin = (left_q+right_q)/2
+    I_ellipse_avg_bin = numpy.average(I_ellipse_bin)
+    
+    qs_bins.append(q_mid_bin)
+    Is_ellipse_bins.append(I_ellipse_avg_bin)
+    
 
 print len(qs), len(Is_ellipse)   
 matplotlib.pyplot.scatter(qs, Is_ellipse, s=3)
+matplotlib.pyplot.scatter(qs_bins, Is_ellipse_bins, s=100, facecolors='c', edgecolors='none', alpha = 0.40)
 matplotlib.pyplot.gca().set_yscale('log')
-matplotlib.pyplot.gca().set_ylim([0.01,1000])
-matplotlib.pyplot.gca().set_xlabel(r'q ($\AA^{-1}$)')
-matplotlib.pyplot.gca().set_ylabel(r'I$_{\rm ellipse}$ (photon counts)', rotation = 'vertical')
+matplotlib.pyplot.gca().set_ylim([0.05, 500])
+matplotlib.pyplot.gca().tick_params(axis='both', which='major', labelsize=12, length=5, pad=7)    
+matplotlib.pyplot.gca().set_xlabel(r'$q$ ($\AA^{-1}$)', fontsize = 18)
+matplotlib.pyplot.gca().set_ylabel(r"I (n photons)", rotation = 'vertical', fontsize = 18)
+matplotlib.pyplot.tight_layout()
 matplotlib.pylab.savefig('%s/I_ellipse_fixed_sigmas_vs_q.png'%outputFolder, dpi=96*4)
 matplotlib.pylab.savefig('%s/I_ellipse_fixed_sigmas_vs_q.pdf'%outputFolder, dpi=96*4)
--- simulate_resolution.py
+++ simulate_resolution.py
@@ -4,9 +4,9 @@ cellSize = 62.45 #62.45
 directCell = cellSize * numpy.matrix([[1, numpy.cos(2*numpy.pi/3)],[0, numpy.sin(2*numpy.pi/3)]]) # A
 reciprocalCellRows = 2* numpy.pi * directCell.I                                                   # A^(-1)
 
-h = 2
-k = 11
-qRod = 0.2412 #0.2991 # A^(-1)   
+h = 3
+k = 4
+qRod = 0 #0.2991 # A^(-1)   
 reciprocalVector = [h, k]*reciprocalCellRows
 q_x = reciprocalVector[0,0]         # A^(-1)
 q_y = reciprocalVector[0,1]         # A^(-1)
